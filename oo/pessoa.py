756d5ad5732154585a960008b65e80057faf0655
BrunoMirandaDev <araujo.bruno@gmail.com>
2020-08-18 13:42:28 -0300

    Criado atributo complexo filhos.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,13 +1,8 @@
-from builtins import filter
-from fnmatch import filter
-
-
 class Pessoa:
     def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
-
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
@@ -20,9 +15,11 @@ if __name__ == '__main__':
     print(id(luciano))
     print(luciano.cumprimentar())
     print(luciano.nome)
-    luciano.nome = 'Bruno'
-    print(luciano.nome)
     print(luciano.idade)
     for filho in luciano.filhos:
         print(filho.nome)
+    luciano.sobrenome = 'Ramalho'
+    del luciano.filhos
+    print(luciano.__dict__)
+    print(renzo.__dict__)
 
